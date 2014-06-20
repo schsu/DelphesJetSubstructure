b87f1997c2e3922d05f88f042f08e9e7e34ff8f5
Milen Lazarov <milenl@uw.edu>
2014-06-19 18:16:53 -0700

    Other things.

--- Extras.C
+++ Extras.C
@@ -5,8 +5,10 @@
 #include <TCanvas.h>
 #include <TH1.h>
 #include <TFile.h>
+#include <TLegend.h>
 
 #include "LocalSettings.h"
+#include "AtlasStyle.h"
 
 void overlapmbb(const std::string& channel) {
     TCanvas* c = new TCanvas("overlapmbb", "overlapmbb", 900, 700);
@@ -28,9 +30,59 @@ void overlapmbb(const std::string& channel) {
     delete c;
 }
 
+void prunedGraphs(const std::string& param, const std::string& coneType) {
+    TCanvas* c = new TCanvas("pruned", "pruned", 900, 700);
+    SetAtlasStyle();
+    char fileName[1024];
+    sprintf(fileName, "%sres-mintree_jetsub_a-zh-%dGeV.root", (const char*)outputFolder, 1500);
+    TFile* f = new TFile(fileName);
+    TLegend* legend = new TLegend(0.7, 0.8, 0.9, 0.7);
+    
+    TH1F* hist = (TH1F*)f->Get((param + ",b" + coneType).c_str());
+    if (hist == NULL) {
+        std::cout << "Histogram not found " << fileName << std::endl;
+        return;
+    }
+
+    std::string title = param + ", " + (coneType == "h" ? "R=1.2" : "R=0.8");
+    title.replace(title.find("bb"), 2, "b");
+    hist->SetTitle(title.c_str());
+    hist->SetMarkerStyle(21);
+    hist->SetMarkerColor(kRed);
+    hist->GetXaxis()->SetTitle("mA, GeV");
+    hist->GetYaxis()->SetTitle("Events           ");
+    legend->AddEntry(hist, "before trimming", "lp");
+    hist->Draw("");
+    
+    TH1F* histp = (TH1F*)f->Get((param + ",b" + coneType + "p").c_str());
+    if (histp == NULL) {
+        std::cout << "Histogram not found " << fileName << std::endl;
+        return;
+    }
+    
+    histp->SetMarkerStyle(22);
+    histp->SetMarkerColor(kBlue);
+    histp->GetXaxis()->SetTitle("mA, GeV");
+    histp->GetYaxis()->SetTitle("Events            ");
+    legend->AddEntry(histp, "after trimming", "lp");
+    histp->Draw("same");
+    
+    legend->Draw("ACp");
+    c->Print(outputFolder + param.c_str() + coneType.c_str() + ".pdf");
+    
+    delete f;
+    delete c;
+}
+
 int main(int argc, char* argv[]) {
     overlapmbb("m(bb),r");
     overlapmbb("m(bb),bl");
     overlapmbb("m(bb),bh");
+    
+    prunedGraphs("m(bb)", "l");
+    prunedGraphs("m(bb)", "l");
+    prunedGraphs("m(bb)", "h");
+    prunedGraphs("pT(bb)", "l");
+    prunedGraphs("pT(bb)", "h");
     return 0;
 }
--- LocalSettings.C
+++ LocalSettings.C
@@ -4,6 +4,7 @@ TString outputFolder = "/Research/outputs/";
 TString inputFolder = "/Research/outputs/";
 TString libDelphesPath = "/phys/groups/tev/scratch3/users/milenl/MadGraph5/Delphes/libDelphes";
 
+//int amasses[] = { 3000 };
 int amasses[] = { 300, 350, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2200, 2400, 2600, 2800, 3000 };
 double xsections[] = { 0.493, 0.0473, 0.0114, 0.00431, 0.00205, 0.00106, 0.000577, 0.000326, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189, 0.000189};
 
